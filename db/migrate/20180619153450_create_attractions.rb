2e72737360f4573a2fd23d8278e90c882ef915f4
newmanbradm <newmanbradm@gmail.com>
2018-06-19 17:59:10 -0400

    seeded database after removing City string column from attractions table

--- db/migrate/20180619153450_create_attractions.rb
+++ db/migrate/20180619153450_create_attractions.rb
@@ -2,7 +2,6 @@ class CreateAttractions < ActiveRecord::Migration[5.0]
   def change
     create_table :attractions do |t|
       t.string :name
-      t.string :city
       t.float :price
       t.boolean :family_friendly
     end

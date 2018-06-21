b9650565090d703c4f6ab1dd7944145df9e5c0c6
newmanbradm <newmanbradm@gmail.com>
2018-06-21 11:52:05 -0400

    fully functional and organized (basic) application with half of the ourREADME.md

--- bin/helper_methods.rb
+++ bin/helper_methods.rb
@@ -66,6 +66,8 @@ end
 
 ## CRUD Methods ##
 
+## Visits CRUD ##
+
 def get_array_of_visits
   $user_traveller.my_visits
 end
@@ -81,24 +83,6 @@ def show_visits
     puts "You do not have any visits to view at this time."
   else
     display_visits
-  end
-end
-
-def get_array_of_reviews
-  $user_traveller.my_reviews
-end
-
-def display_reviews
-  get_array_of_reviews.each do |review_instance|
-    puts "Review of #{review_instance.attraction.name} in #{review_instance.attraction.city.name} -- Rating: #{review_instance.rating}, Message: #{review_instance.message}"
-  end
-end
-
-def show_reviews
-  if $user_traveller.my_reviews == []
-    puts "You do not have any reviews to view at this time."
-  else
-    display_reviews
   end
 end
 
@@ -137,6 +121,26 @@ def delete_visit
     id_to_delete = user_choice_visit.split(", ")[0].to_i
     Visit.all.find(id_to_delete).destroy
     puts "Your visit has been deleted!"
+  end
+end
+
+## Reviews CRUD ##
+
+def get_array_of_reviews
+  $user_traveller.my_reviews
+end
+
+def display_reviews
+  get_array_of_reviews.each do |review_instance|
+    puts "Review of #{review_instance.attraction.name} in #{review_instance.attraction.city.name} -- Rating: #{review_instance.rating}, Message: #{review_instance.message}"
+  end
+end
+
+def show_reviews
+  if $user_traveller.my_reviews == []
+    puts "You do not have any reviews to view at this time."
+  else
+    display_reviews
   end
 end
 
---
+++ ourREADME.md
@@ -0,0 +1,20 @@
+# Attract-US
+
+Welcome to Attract-US, an interactive Command Line Interface application for attractions in five major US cities!
+
+## Description
+
+Attract-US is a Command Line Interface application that allows users to plan a visit to, or write a review of, popular attractions across the United States. Users have full access to create, view, update, and delete their own visits and reviews.   
+
+## Instructions
+
+1. Fork and clone this repository to your local terminal.
+2. CD into the "module-one-final-project-guidelines-nyc-web-060418" directory.
+3. Input "bundle install" into the terminal and press enter.
+4. Input "ruby bin/run.rb" into the terminal and press enter to start the application.
+5. Follow the instructions on the screen to interact with the application.
+6. Enjoy!
+
+## Contributors Guide
+
+## Licensing 
